b0e8eab2556237580344a9bed549394f85a18cc6
Kaleo <kaleocheng@gmail.com>
2019-05-16 12:26:52 +0800

    Rename index.js to cli.js

--- cli.js
+++ cli.js
@@ -3,17 +3,25 @@ const render = require('./render')
 const parser = require('./parser')
 const util = require('util')
 const commander = require('commander')
+const pkg = require('./package.json')
 
 
 commander
-    .option('--filter [filter]', 'Filter crafts')
-    .option('--show-object', 'Just show object, do not render')
+    .name('craftdot')
+    .version(pkg.version)
+    .option('--filter [*]', 'filter crafts, support wildcard such as service* (default is *)')
+    .option('--show-object', 'just show object, do not render')
     .parse(process.argv);
+
+if (!commander.args.length) {
+    commander.outputHelp()
+    process.exit()
+}
 
 const craftdots = commander.args.map(f => {
     if (!fs.existsSync(f)) {
         console.log(`${f} is not exist`)
-        exit()
+        process.exit()
     }
     return fs.readFileSync(f, 'utf8')
 })
