8b1922e8e1413db9dd8adce6ba45001599aee041
Kaleo <kaleocheng@gmail.com>
2019-05-16 15:18:43 +0800

    Support show diagram in browser

--- cli.js
+++ cli.js
@@ -5,12 +5,15 @@ const render = require('./render')
 const parser = require('./parser')
 const util = require('util')
 const commander = require('commander')
+const open = require('open')
+const tmp = require('tmp')
 const pkg = require('./package.json')
 
 
 commander
     .name('craftdot')
     .version(pkg.version)
+    .option('--show-in-browser', 'show diagram in default brower')
     .option('--filter [*]', 'filter crafts, support wildcard such as service* (default is *)')
     .option('--show-object', 'just show object, do not render')
     .parse(process.argv);
@@ -31,7 +34,36 @@ const craftdots = commander.args.map(f => {
 const crafts = parser.parse(craftdots, commander.filter)
 if (commander.showObject) {
     console.log(util.inspect(crafts, false, null, true))
-} else {
-    output = render.render_diagraph(crafts)
-    console.log(output)
+    process.exit()
 }
+const output = render.render_diagraph(crafts)
+
+if (commander.showInBrowser) {
+    let html = `
+    <!DOCTYPE html>
+    <meta charset="utf-8">
+
+    <body>
+        <script src="https://d3js.org/d3.v4.min.js"></script>
+        <script src="https://unpkg.com/viz.js@1.8.0/viz.js" type="javascript/worker"></script>
+        <script src="https://unpkg.com/d3-graphviz@1.4.0/build/d3-graphviz.min.js"></script>
+        <div id="graph" style="text-align: center;"></div>
+        <script>
+            d3.select("#graph").graphviz()
+                .fade(false)
+                .renderDot(\`${output}\`);
+        </script>
+    </body>
+    `
+    let htmlFile = tmp.fileSync({
+        postfix: '.html'
+    })
+    fs.writeFileSync(htmlFile.name, html);
+    (async () => {
+        await open(`file://${htmlFile.name}`);
+    })();
+    htmlFile.removeCallback()
+    process.exit()
+}
+
+console.log(output)
